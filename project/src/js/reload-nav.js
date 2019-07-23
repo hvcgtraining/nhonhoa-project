fa464fcc5c14a47690da18b53bc55d8e91b5eb53
HVCG <hvcgtraining@gmail.com>
2019-07-23 14:07:23 +0700

    add

--- project/src/js/reload-nav.js
+++
@@ -1,3 +0,0 @@
-	$('.navigation').find('li.active').parents('li').addClass('active');
-	$('.navigation').find('li').not('.active').has('ul').children('ul').addClass('hidden-ul');
-	$('.navigation').find('li').has('ul').children('a').parent('li').addClass('has-ul');
