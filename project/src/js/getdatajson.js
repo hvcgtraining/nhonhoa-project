bf4faac2585d3220db19387352a20f658556e707
HVCG <hvcgtraining@gmail.com>
2019-07-29 11:01:13 +0700

    updated SRC

---
+++ project/src/js/getdatajson.js
@@ -0,0 +1,49 @@
+
+
+$(function(){
+    $(document).ready(function(){
+        $.ajax({
+            url: 'http://localhost:3000/dinhmucvattu',
+            type: 'GET',
+            success: function (data, textStatus, jQxhr) {
+                var _data = data;
+                console.log(JSON.stringify(_data));
+                var html = "";
+                $.each(_data,function(i,val){
+                    html += `<tr><td><div class="btn-control row"><button class="col-6"><i class="fas fa-edit"></i></button><button class="col-6"><i class="fas fa-trash-alt"></td><td>${val.sochungtu}</td><td>${val.ngaylap}</td><td>${val.mathanhpham}</td><td>${val.tenthanhpham}</td><td>${val.macapphat}</td><td>${val.loai}</td><td>${val.khoqt}</td></tr>`
+                })
+                $("#dinhmucvattusanxuat_table table tbody").html(html);
+                console.log(textStatus);
+                console.log(jQxhr.responseJSON);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+                console.log(jqXhr.responseJSON.message);
+            }
+        })
+    })
+})
+
+$(function(){
+    $(document).ready(function(){
+        $.ajax({
+            url: 'http://localhost:3000/chitietdinhmuc',
+            type: 'GET',
+            success: function (data, textStatus, jQxhr) {
+                var _data = data;
+                console.log(JSON.stringify(_data));
+                var html = "";
+                $.each(_data,function(i,val){
+                    html += `<tr><td><div class="btn-control row"><button class="col-6"><i class="fas fa-edit"></i></button><button class="col-6"><i class="fas fa-trash-alt"></td><td>${val.machitiet}</td><td>${val.tenchitiet}</td><td>${val.slcp}</td><td>${val.ghichu}</td></tr>`
+                })
+                $("#taodinhmucvattu-ketiep__table table tbody").html(html);
+                console.log(textStatus);
+                console.log(jQxhr.responseJSON);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+                console.log(jqXhr.responseJSON.message);
+            }
+        })
+    })
+})
---
+++ project/src/js/navigation.js
@@ -0,0 +1,11 @@
+
+var clicka = document.getElementById("sidebar-toggle");
+clicka.addEventListener("click",function(){
+    var administrator = document.getElementById("administrator");
+    administrator.classList.toggle("menu");
+});
+function setwidth(){
+    var widthsidebar = $(".sidebar").width();
+    console.log(widthsidebar);
+    $("li#administrator").width(widthsidebar);
+}
